202c606c1e8d4f1caba9a822395a4215c1edfd9d
タイン・ヴィン <117455879+vinh12112004@users.noreply.github.com>
2024-10-21 14:21:42 +0700

    Add files via upload

---
+++ AIMS/Cart.java
@@ -0,0 +1,32 @@
+public class Cart{
+    public static final int MAX_NUMBER_ORDERED=20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+    int qtyOrdered = 0;
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){
+        if(qtyOrdered < MAX_NUMBER_ORDERED - 1){
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("them thanh cong");
+        } else System.out.println("gio hang day ko them duoc");
+    }
+    public void deleteDigitalVideoDisc(DigitalVideoDisc disc){
+        for(int i=0; i <= qtyOrdered; i++){
+            if (itemsOrdered[i].equals(disc)){
+                for (int j=i; j<qtyOrdered; j++){
+                    itemsOrdered[j]=itemsOrdered[j+1];
+                }
+            }
+            itemsOrdered[qtyOrdered]=null;
+            qtyOrdered--;
+            System.out.println("xoa thanh cong");
+        }
+        System.out.println("dia ko co de xoa");
+    }
+    public int totalCost(){
+        int total=0;
+        for(int i=0; i<=qtyOrdered; i++){
+            total+=itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+}
