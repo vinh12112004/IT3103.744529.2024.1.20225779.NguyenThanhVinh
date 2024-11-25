0be9125e2e862c558ef79efeb42f48996f7b750e
タイン・ヴィン <117455879+vinh12112004@users.noreply.github.com>
2024-11-25 13:02:31 +0700

    Delete Lab03 directory

--- Lab03/GarbageCreator.java
+++
@@ -1,9 +0,0 @@
-public class GarbageCreator {
-    public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 100000; i++) {
-            String s = "garbage" + i;
-        }
-        System.out.println(System.currentTimeMillis() - start);
-    }
-}
